2b9b53d14728370ce5b3b0be5f7d3ee34e4a2e7e
Iris <iris.wolfisberg@hotmail.fr>
2019-05-25 15:15:59 +0200

    controller cocktails

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,4 +2,5 @@ class Cocktail < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true, uniqueness: true
+  validates :photo, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'cocktails#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [:index, :show]
 end
