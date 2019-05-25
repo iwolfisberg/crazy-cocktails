fbb10155b188f3b2eec6218ffe75f3ac11858be8
Iris <iris.wolfisberg@hotmail.fr>
2019-05-25 21:36:52 +0200

    Changed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,8 @@ require 'json'
 puts "Destroy all"
 User.destroy_all
 Cocktail.destroy_all
+Ingredient.destroy_all
+Dose.destroy_all
 
 # ----------------------------------------------------------------------
 
@@ -29,6 +31,18 @@ users = [
 
 # ----------------------------------------------------------------------
 
+puts "Create ingredients"
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredient_serialized = open(url).read
+ingredient = JSON.parse(ingredient_serialized)["drinks"]
+
+ingredient.each do |ing|
+  Ingredient.create!(name: ing["strIngredient1"])
+end
+
+# ----------------------------------------------------------------------
+
 puts "Create cocktails"
 
 url = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail'
@@ -36,5 +50,18 @@ cocktail_serialized = open(url).read
 cocktails = JSON.parse(cocktail_serialized)["drinks"]
 
 cocktails.each do |cocktail|
-  Cocktail.create!(name: cocktail["strDrink"], remote_photo_url: cocktail["strDrinkThumb"], user_id: users.sample[:id])
+  new_cocktail = Cocktail.create!(name: cocktail["strDrink"], remote_photo_url: cocktail["strDrinkThumb"], user_id: users.sample[:id])
+
+  id = cocktail["idDrink"]
+  url_cocktail = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=#{id}"
+  cocktail_id_serialized = open(url_cocktail).read
+  cocktail_id = JSON.parse(cocktail_id_serialized)["drinks"][0]
+
+  puts "Create first dose"
+  Dose.create!(quantity: cocktail_id["strMeasure1"], ingredient_id: Ingredient.where(name: cocktail_id["strIngredient1"])[0].id, cocktail_id: new_cocktail[:id])
+  puts "Create second dose"
+  Dose.create!(quantity: cocktail_id["strMeasure2"], ingredient_id: Ingredient.where(name: cocktail_id["strIngredient2"])[0].id, cocktail_id: new_cocktail[:id])
 end
+
+# ----------------------------------------------------------------------
+
