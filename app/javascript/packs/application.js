a6c1ae48659fc7b3ad452bcd8c1cc63b0a6e2383
Iris <iris.wolfisberg@hotmail.fr>
2019-05-25 23:13:15 +0200

    A bit of layout

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,3 +2,7 @@ import "bootstrap";
 
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 initUpdateNavbarOnScroll();
+
+import 'select2/dist/css/select2.css';
+import { initSelect2 } from '../components/init_select2';
+initSelect2();
