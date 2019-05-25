cbefea768160a8d8a8a8681123e0789e228f2407
Iris <iris.wolfisberg@hotmail.fr>
2019-05-25 18:38:03 +0200

    Basic and responsive layout

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -2,8 +2,10 @@ class CocktailsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
+    @cocktails = Cocktail.all
   end
 
   def show
+    @cocktail = Cocktail.find(params[:id])
   end
 end
