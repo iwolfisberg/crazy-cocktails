782c3d36ec97bc09c303621d58140df3d86acedd
Iris <iris.wolfisberg@hotmail.fr>
2019-05-26 13:22:28 +0200

    Modified schema migration

---
+++ db/migrate/20190526111845_drop_recipe_table.rb
@@ -0,0 +1,5 @@
+class DropRecipeTable < ActiveRecord::Migration[5.2]
+  def change
+    drop_table :recipes
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_25_211503) do
+ActiveRecord::Schema.define(version: 2019_05_26_112105) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_05_25_211503) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
     t.index ["user_id"], name: "index_cocktails_on_user_id"
   end
 
@@ -40,14 +41,6 @@ ActiveRecord::Schema.define(version: 2019_05_25_211503) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "recipes", force: :cascade do |t|
-    t.string "description"
-    t.bigint "cocktail_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["cocktail_id"], name: "index_recipes_on_cocktail_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -63,5 +56,4 @@ ActiveRecord::Schema.define(version: 2019_05_25_211503) do
   add_foreign_key "cocktails", "users"
   add_foreign_key "doses", "cocktails"
   add_foreign_key "doses", "ingredients"
-  add_foreign_key "recipes", "cocktails"
 end
