ec24d70fc51f77f29129f4066cce337918d2d80a
Iris <iris.wolfisberg@hotmail.fr>
2019-06-12 08:36:44 +0200

    Add review form to cocktail show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+  get 'reviews/create'
   devise_for :users
   root to: 'cocktails#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [:index, :show, :new, :create]
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:new, :create]
+  end
 end
